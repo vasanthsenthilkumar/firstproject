904a388e0d21a7ef7629065b7f575ffa219e4d3f
vasanthsenthilkumar <vasanthedison@gmail.com>
2019-04-30 13:27:01 +0530

    add the code

---
+++ firstproject/src/firstproject/firstnew.java
@@ -0,0 +1,17 @@
+package firstproject;
+
+public class firstnew {
+
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4};
+		int sum = 0;
+		
+		for (int i : a) {
+			sum += i;
+			
+		}
+		System.out.print(sum);
+
+	}
+
+}
